0f31e2044fea24446eae12813e4924f3e77174e3
YunChongKim <148053155+YunChongKim@users.noreply.github.com>
2023-11-12 14:21:57 +0900

    Update 머쓱이보다 키 큰 사람.py

--- 프로그래머스/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/머쓱이보다 키 큰 사람.py
@@ -1,7 +1,9 @@
 def solution(array, height):
     i =0
     result =0
-    for i in range(len(array)):
+    for i in range(len(array)): 
+        # 그냥 array로 돌아도 됐음.
         if array[i] > height:
+        #그냥 i로 적어도 됐음.    
             result +=1
     return result
